007aec29c53f6927935512ce5d132ddbb6816fcb
LenaRib <poddubnayaeo@gmail.com>
2021-02-07 07:09:40 +0100

    Add JSON parser and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,22 @@
-#!usr/bin/env node
+#!/usr/bin/env node
+import program from 'commander';
+import genDiff from '../src/parser.js';
 
-const commander = require('commander');
-const programm = new commander.Command();
+program
+  .usage('[options] <filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action(
+    (filepath1, filepath2) => {
+      genDiff(filepath1, filepath2);
+    },
+  )
+  .parse(process.argv);
 
-programm
-    .option('-V', '--version', 'output the version number')
-    .option('-h', '--help', 'output usage information');
+const options = program.opts();
+if (options.help) {
+  console.log('Options: ', program.opts());
+}
